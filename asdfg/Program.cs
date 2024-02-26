790bed691d7df46d6267e2bd7a2cc92d0d899ef5
Dmitrii Bernikov <bernikov.dmitrii05@gmail.com>
2024-02-27 00:40:33 +0300

    библиотека маф

--- asdfg/Program.cs
+++ asdfg/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello!");
+﻿using System.Globalization;
+
+int x, y;
+x = 25;
+y = 5;
+Console.WriteLine($"Корень из x - {Math.Sqrt(x)}");
+Console.WriteLine($"Логарифм из x - {Math.Log(x)}");
+Console.WriteLine($"Квадрат из x - {Math.Pow(x,2)}");
